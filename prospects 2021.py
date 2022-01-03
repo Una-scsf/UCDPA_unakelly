6a394cf2ab75fc912f539d19921f1d43959084a4
Una-scsf <81979330+Una-scsf@users.noreply.github.com>
2022-01-03 19:47:07 +0000

    Ordering and identify dups & missing values
    Dataframe was ordered by score, highest to lowest
    
    Duplicates were identified and dropped
    
    Missing values were identied

--- prospects 2021.py
+++ prospects 2021.py
@@ -2,3 +2,15 @@ import pandas as pd
 prospects = pd.read_csv("/Users/ukelly/Desktop/UCDPA_una/Prospects_January_2022.csv")
 print(prospects.head())
 print(prospects.info())
+
+prospects.sort_values("Score", ascending=False)
+
+print(prospects["Email"].value_counts())
+print(prospects.isna().any())
+
+prospects.drop_duplicates(subset="Email")
+
+import matplotlib.pyplot as plt
+prospects.isna().sum().plot(kind="bar", rot = 45, title = "Columns with Missing Values")
+plt.show()
+plt.clf()
