bce336086a88781cc68ce491275700dd4f05a2bb
Una-scsf <81979330+Una-scsf@users.noreply.github.com>
2022-01-03 22:27:08 +0000

    Merge dataframes
    Merge unq prospects dataframe with hot prospects dataframe

--- prospects 2021.py
+++ prospects 2021.py
@@ -3,24 +3,28 @@ prospects = pd.read_csv("/Users/ukelly/Desktop/UCDPA_una/Prospects_January_2022.
 print(prospects.head())
 print(prospects.info())
 
-ordered_prospects = prospects.sort_values("Score", ascending=False)
+dup_prospects = prospects.sort_values("Score", ascending=False)
 
-print(ordered_prospects["Email"].value_counts(sort=True))
+print(dup_prospects["Email"].value_counts(sort=True))
 
-ordered_prospects.drop_duplicates(subset="Email")
+unq_prospects = dup_prospects.drop_duplicates(subset="Email")
 
-print(ordered_prospects.isna().any())
+print(unq_prospects.isna().any())
 
 import matplotlib.pyplot as plt
-ordered_prospects.isna().sum().plot(kind="bar", rot = 45, title = "Columns with Missing Values")
+unq_prospects.isna().sum().plot(kind="bar", rot = 45, title = "Columns with Missing Values")
 plt.show()
 plt.clf()
 
-ordered_prospects["Grade"].fillna("D", inplace=True)
+unq_prospects["Grade"].fillna("D", inplace=True)
 
-ordered_prospects.isna().sum().plot(kind="bar", rot = 45, title = "Updated Columns with Missing Values")
+unq_prospects.isna().sum().plot(kind="bar", rot = 45, title = "Updated Columns with Missing Values")
 plt.show()
 plt.clf()
 
-ordered_prospects["Full Name"] = ordered_prospects["First Name"] + " " + ordered_prospects["Last Name"]
-print(ordered_prospects)
+unq_prospects["Full Name"] = unq_prospects["First Name"] + " " + unq_prospects["Last Name"]
+print(unq_prospects)
+
+hot_prospects = pd.read_csv("/Users/ukelly/Desktop/UCDPA_una/Hot_Prospects_2022.csv")
+unq_with_hot_prospects = pd.merge(unq_prospects, hot_prospects, on=["Prospect Id", "Email"], how="outer")
+print(unq_with_hot_prospects.info())
