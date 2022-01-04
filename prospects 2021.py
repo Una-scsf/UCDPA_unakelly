28c2def3b5a6c8bc0fedbb3a607a1f66d1dc1cec
Una-scsf <81979330+Una-scsf@users.noreply.github.com>
2022-01-04 23:17:22 +0000

    Field type updates

--- prospects 2021.py
+++ prospects 2021.py
@@ -1,8 +1,10 @@
 import pandas as pd
-prospects = pd.read_csv("/Users/ukelly/Desktop/UCDPA_una/Prospects_January_2022.csv")
+prospects = pd.read_csv("/Users/ukelly/Desktop/UCDPA_unakelly/Prospects_January_2022.csv")
 print(prospects.head())
 print(prospects.info())
 
+prospects["Last Activity Date"] = pd.to_datetime((prospects["Last Activity Date"]))
+prospects["Created Date"] = pd.to_datetime((prospects["Created Date"]))
 prospects["First Name"] = prospects["First Name"].str.capitalize()
 prospects["Last Name"] = prospects["Last Name"].str.capitalize()
 prospects["Full Name"] = prospects["First Name"] + " " + prospects["Last Name"]
@@ -28,17 +30,10 @@ plt.clf()
 
 print(unq_prospects)
 
-hot_prospects = pd.read_csv("/Users/ukelly/Desktop/UCDPA_una/Hot_Prospects_2022.csv")
+hot_prospects = pd.read_csv("/Users/ukelly/Desktop/UCDPA_unakelly/Hot_Prospects_2022.csv")
 unq_with_hot_prospects = pd.merge(unq_prospects, hot_prospects, on=["Prospect Id", "Email"], how="outer")
-
-unq_with_hot_prospects["Last Activity Date"] = pd.to_datetime((unq_with_hot_prospects["Last Activity Date"]))
-unq_with_hot_prospects["Created Date"] = pd.to_datetime((unq_with_hot_prospects["Created Date"]))
 
 unq_with_hot_prospects.reset_index()
 
 print(unq_with_hot_prospects.info())
-
-unq_with_hot_prospects["Grade"].hist()
-plt.show()
-plt.clf()
 
